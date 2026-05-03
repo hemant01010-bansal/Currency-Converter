bdfaf1a6300aa51cf54d0f734245e2535a040d16
hemant0101 <iamhemant456@gmail.com>
2026-05-03 11:42:13 +0530

    add git

---
+++ script.js
@@ -0,0 +1,215 @@
+//INR-->currency code
+//IN-->country code
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+
+const dropdowns=document.querySelectorAll(".convert select");
+
+for (select of dropdowns){
+    for (currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
+
+}
+
+const updateFlag=(element)=>{
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
+    let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img=element.parentElement.querySelector("img");
+    img.src=newSrc;
+}
+
+async function calcuExchangeRate(){
+    let value=document.querySelector(".inputValue").value;
+    let result=document.querySelector(".result");
+    let fromCurrCode=document.querySelector(".from .selectContainer select").value;
+    let toCurrCode=document.querySelector(".to .selectContainer select").value;
+    let response=await fetch(`https://fxapi.app/api/${fromCurrCode}/${toCurrCode}.json`);
+    let data=await response.json();
+    let calcValue=value*data.rate;
+    result.innerText=`${value} ${fromCurrCode} = ${calcValue} ${toCurrCode}`;
+}
+
+let getResult=document.querySelector(".getResult");
+console.log(getResult);
+getResult.addEventListener("click",calcuExchangeRate);
+
+const rotater=()=>{
+    let fromCurrCode=document.querySelector(".from .selectContainer select").value;
+    let toCurrCode=document.querySelector(".to .selectContainer select").value;
+    let fromCountryCode=countryList[fromCurrCode];
+    console.log(fromCountryCode);
+    let toCountryCode=countryList[toCurrCode];
+    console.log(toCountryCode);
+    let fromimg=document.querySelector(".from .selectContainer img");
+    let toimg=document.querySelector(".to .selectContainer img");
+    fromimg.src=`https://flagsapi.com/${toCountryCode}/flat/64.png`;
+    toimg.src=`https://flagsapi.com/${fromCountryCode}/flat/64.png`;
+}
+console.log(countryList.USD);
